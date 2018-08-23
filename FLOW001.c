6746745c3f89cf5cd49dba401e4c3ff00ff7df4b
Tharun <tharunravuri2000@gamil.com>
2018-08-23 17:37:30 +0530

    Addittion

---
+++ FLOW001.c
@@ -0,0 +1,15 @@
+/* this code is for addittion in codechef problem FLOW001*/
+#include<stdio.h>
+int main()
+{
+    int i,n,a,b,c;
+   
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       scanf("%d %d\n",&a,&b);
+    c=a+b;
+    printf("%d\n",c);
+    }
+    return 0;
+}
