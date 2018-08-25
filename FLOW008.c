8456b76020429bdc9c3ad63e1939640ade0d541f
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2018-08-26 00:47:51 +0530

    Add files via upload

---
+++ FLOW008.c
@@ -0,0 +1,21 @@
+/* this code is for servant problem in code chef problem no FLOW003 */
+#include<stdio.h>
+int main()
+{
+    int i,n,a;
+   
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+      scanf("%d\n",&a);
+      if(a>=10)
+      {
+          printf("-1\n");
+      }
+      else
+      {
+          printf("What an obedient servant you are!\n");
+      }
+    }
+    return 0;
+}
