aae6cb3089f0d5e2a938e3bfbcc7ae59f467fec4
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2018-08-26 22:38:46 +0530

    Add files via upload
    This code is for Id and ship in code chef problem no FLOW010

---
+++ FLOW010.c
@@ -0,0 +1,30 @@
+/* this code is for Id and Ship in codechef problem no FLOW010*/
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    char a;
+   
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+      scanf("%c\n",&a);
+      
+      if(a=='B'||a=='b')
+      {
+          printf("BattleShip\n");
+      }
+      else if(a=='C'|| a=='c')
+      {
+          printf("Cruiser\n");
+      }
+      else if(a=='D'|| a=='d')
+      {
+          printf("Destroyer\n");
+      }
+      else if(a=='F'|| a=='f')
+      {
+          printf("Frigate\n");
+      }
+    }
+}
