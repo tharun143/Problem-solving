2f43c4b2ddb38d346757e717855fd9e1207d5ea3
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2018-08-29 22:48:38 +0530

    Copy-Paste in Code Chef problem no RRCOPY
    this code is for Copy-paste in code chef problem no RRCOPY

---
+++ RRCOPY.c
@@ -0,0 +1,26 @@
+/* this code is for Copy-Paste in code chef problem no RRCOPY*/ 
+#include<stdio.h>
+int main()
+{
+int m,i,j,d,t;
+scanf("%d",&t);
+while(t--)
+{
+d=0;
+scanf("%d",&m);
+int a[1000000];
+for(i=0;i<1000000;i++)
+a[i]=0;
+for(i=0;i<m;i++)
+{scanf("%d",&j);
+a[j]=1;
+}
+for(i=0;i<1000000;i++)
+if(a[i]==1)
+d++;
+printf("%d\n",d);
+}
+return 0;
+}
+
+
