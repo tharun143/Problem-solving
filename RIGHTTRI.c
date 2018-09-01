090eaeee74167ddcc30da53a7050b77352f08590
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2018-09-01 23:30:47 +0530

    Problem no: RIGHTTRI in code chef

---
+++ RIGHTTRI.c
@@ -0,0 +1,22 @@
+/* this code is for Right Triangle in code chef problem no RIGHTTRI */
+#include <stdio.h>
+    #include <math.h>
+    #define ll long long
+    int main()
+    {
+    	int m,h;
+    	ll s,a,b;
+    	scanf("%d",&m);
+    	while(m--)
+    	{
+    		scanf("%d%lld",&h,&s);
+    		a=(ll)h*h-4*s;
+    		if(a>=0)
+    		{
+    			b=(ll)h*h+4*s;
+    			printf("%lf %lf %lf\n",(sqrt(b)-sqrt(a))/2,(sqrt(b)+sqrt(a))/2,(double)h);
+            }
+            else printf("-1\n");
+        }
+        return 0;
+    } 
