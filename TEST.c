e693f76c8a352accc97472a24eb669e1584ae9dd
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2018-09-01 23:46:34 +0530

    Life , The Universe and Everything prob: TEST

---
+++ TEST.c
@@ -0,0 +1,18 @@
+/* this code is for Life ,The Universe and Everything */
+
+#include<stdio.h>
+int main()
+{
+int b;
+while(1)
+{
+scanf("%d",&b);
+if(b!=42)
+{
+printf("%d\n",b);
+}
+else
+break;
+}
+return 0;
+}
