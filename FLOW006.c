4b95b911d8846219b75ce74db7947ce63f5b1378
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2018-09-04 21:35:44 +0530

    Sum of digits Flow 007

---
+++ FLOW006.c
@@ -0,0 +1,20 @@
+/* this code is for sum of digits in code chef priblem no FLOW006 */
+#include<stdio.h>
+int main()
+{
+    int t,i,a,s,n;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++)
+    {
+        scanf("%d",&n);
+        s=0;
+        while(n>0)
+        {
+            a=n%10;
+            s=s+a;
+            n=n/10;
+        }
+        printf("%d\n",s);
+    }
+   
+}
